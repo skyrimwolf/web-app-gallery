7b04317a579074ce7ba4748baa8de271bcae67f4
Aleksandar Cernicin <aleksandar.cernicin@gmail.com>
2024-01-31 03:50:23 +0100

    added part of replay logs

--- server/test/replayLogs.js
+++ server/test/replayLogs.js
@@ -1,6 +1,9 @@
 const expect = require('chai').expect
+const sinon = require('sinon')
 
+const imageController = require('../controllers/image')
 const logger = require('../controllers/logger')
+const Logger = require('../models/logger')
 
 //I will use these times for tests
 const startTime = new Date('2024-01-30T17:51:30.584Z')
@@ -32,9 +35,41 @@ describe('Tests for creating crops', () => {
 
  describe('Tests for replaying crops', () => {
     it ('should replay logs successfully', async () => {
+        const logSpy = sinon.spy(Logger, 'log') //mocks a Logger.log() function so it doesn't write in the log file during testing
+        
         const dummyReq = [
             {filename: '1706636402396_1706632397961_tigar.jpg'},
             {filename: '1706637096048_1706632397961_tigar.jpg'}
         ]
+
+        const logs = await logger.makeCropLog(startTime, endTime) //5 rows
+
+        let previousTimestamp = null //needed to get time
+
+        logs.forEach((log) => {
+            const currentTimestamp = new Date(log.timestamp).getTime();
+            const delay = previousTimestamp ? currentTimestamp - previousTimestamp : 0 //ternary operator, if prev is null, delay is 0
+            const functionName = log.message.substring(0, log.message.indexOf('('))
+
+            setTimeout(async () => {
+                switch (functionName) {
+                    case 'getAllImages':
+                        const result = await imageController.getGetAllImageNames({}, {}, () => {})
+                        expect(result).to.have.property('filename')
+                        break
+                    case 'postUploadImage':
+                        break
+                    case 'postRotateImage':
+                        break
+                    case 'deleteRemoveImage':
+                        break
+                }
+                
+            }, delay) //code inside of setTimeout done after delay
+
+            previousTimestamp = currentTimestamp
+        })
+
+        logSpy.restore() //restore the original Logger.log() so it runs smoothly after testing
     })
  })
